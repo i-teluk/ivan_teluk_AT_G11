cb0ce08acee3336389b80279b6c0d1b3ca5c5d03
ivan <i.telyuk@lerna.tech>
2024-11-07 22:07:58 +0300

    add homework

---
+++ src/basetask/TrainMethodsStringRunner.java
@@ -0,0 +1,11 @@
+package basetask;
+
+public class TrainMethodsStringRunner {
+    public static void main(String[] args) {
+        TrainMethodsString trainMethodsString = new TrainMethodsString();
+        trainMethodsString.printMama();
+        trainMethodsString.printPapa();
+        trainMethodsString.printString("строка на входе");
+        trainMethodsString.printMamaString("окна");
+    }
+}
