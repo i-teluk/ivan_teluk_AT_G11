0ebe543ac50cfa513ec3682090a47b5fd95df47c
ivan <i.telyuk@lerna.tech>
2024-11-15 23:43:30 +0300

    feat(homework 5): string task

---
+++ src/main/java/homework/day5/string/IpStatus.java
@@ -0,0 +1,13 @@
+package main.java.homework.day5.string;
+
+public class IpStatus {
+    public void ipstatus (String log) {
+        String[] str = log.split("\\n");
+        for (int i = 0; i < str.length; i++) {
+
+
+        }
+
+
+    }
+}
