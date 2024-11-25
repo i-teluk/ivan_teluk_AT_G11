d402bea42b1547bdeee2154110594dc6923bacca
ivan <i.telyuk@lerna.tech>
2024-11-25 22:15:11 +0300

    add homework

---
+++ src/main/java/homework/day7/Cars.java
@@ -0,0 +1,38 @@
+package main.java.homework.day7;
+
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    public static void main(String[] args) {
+        List<String> cars = new ArrayList<>();
+        String[] carsArray = "Мерс, Ауди, Жигуль, Рено, Жигуль, Жигуль, Ауди".split(", ");
+        for (String car : carsArray) {
+            cars.add(car);
+        }
+
+        String fileName = "cars.txt";
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (String car : cars) {
+                writer.write("\"" + car + "\"" + "\n");
+            }
+            System.out.println("Файл записан");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < cars.size(); i++) {
+            String[] carArray = cars.get(i).split("");
+            if (carArray.length > 4) {
+                cars.remove(i);
+                i--;
+            }
+        }
+
+        for (String car : cars) {
+            System.out.print(car + " ");
+        }
+    }
+}
---
+++ src/main/java/homework/day7/Countries.java
@@ -0,0 +1,35 @@
+package main.java.homework.day7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Countries {
+    public static void main(String[] args) {
+        List<String> countries = new ArrayList<>();
+        countries.add("Андора");
+        countries.add("Португалия");
+        countries.add("Англия");
+        countries.add("Замбия");
+
+        for (String country : countries) {
+            System.out.print(country);
+            if (!countries.get(countries.size() - 1).equals(country)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        int count = 0;
+        for (String country : countries) {
+            String[] split = country.split("");
+            if (split.length < 7) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        for (int i = 0; i < countries.size(); i++) {
+            System.out.println(countries.get(i));
+        }
+    }
+}
