418cd523431ff7f726a07ba0a838579f6f8a974a
ivan <i.telyuk@lerna.tech>
2024-12-03 00:18:05 +0300

    feat(homework): add hometask day 9

---
+++ src/main/homework/day9/BirdsRunner.java
@@ -0,0 +1,13 @@
+package main.homework.day9;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BirdsRunner {
+    public static void main(String[] args) {
+
+        List<String> birds = Arrays.asList("Чайка","Дрозд","Бусел","Голубь","Воробей","Цапля");
+        Arrays.stream(birds.stream().map(bird -> bird.replace("о", "а")).collect(Collectors.joining()).toLowerCase().replace("ь","").split("б")).map(bird -> "--" + bird + "--").forEach(System.out::println);
+    }
+}
---
+++ src/main/homework/day9/FiguresRunner.java
@@ -0,0 +1,10 @@
+package main.homework.day9;
+
+import java.util.stream.Stream;
+
+public class FiguresRunner {
+    public static void main(String[] args) {
+        Stream<String> figures = Stream.of("Овал","Прямоугольник","Круг","Квадрат","Эллипс");
+        figures.map(s -> s.length()).filter(s -> s > 4).forEach(System.out::println);
+    }
+}
