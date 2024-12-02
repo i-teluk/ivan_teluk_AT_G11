db6980c6f07811de2cb17d3ceace493662f50270
ivan <i.telyuk@lerna.tech>
2024-12-02 21:55:55 +0300

    feat(classwork): day 9

---
+++ src/main/classwork/day9/ClassX.java
@@ -0,0 +1,7 @@
+package main.classwork.day9;
+
+public class ClassX {
+    public ClassX(String x) {
+        System.out.println("I am the instance of X with: " + x);
+    }
+}
---
+++ src/main/classwork/day9/MyClass.java
@@ -0,0 +1,7 @@
+package main.classwork.day9;
+
+public class MyClass {
+    public void justMethod(IMyInterface staff) {
+        staff.doSomething("The result of something");
+    }
+}
