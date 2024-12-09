d6a181e313bc1fcfc517ed18583f5ec788235943
ivan <i.telyuk@lerna.tech>
2024-12-09 22:23:14 +0300

    feat(homework): add homework day 11

--- src/main/homework/playground/essence/craft/hand/Storable.java
+++ src/main/homework/playground/essence/craft/hand/Storable.java
@@ -1,4 +1,7 @@
 package main.homework.playground.essence.craft.hand;
 
+import main.homework.playground.essence.material.Pourable;
+
 public interface Storable {
+    void store(Pourable pourable, int amount);
 }
--- src/main/homework/playground/essence/material/Petrol.java
+++ src/main/homework/playground/essence/material/Petrol.java
@@ -1,4 +1,16 @@
 package main.homework.playground.essence.material;
 
-public class Petrol implements Pourable, Powerable {
+import main.homework.playground.essence.Matter;
+
+public class Petrol extends Matter implements Pourable, Powerable {
+
+    public Petrol(int mass) {
+        super(mass);
+    }
+
+    @Override
+    public void spread(String storeName) {
+        System.out.printf("I am %s and I am spreading in %s\n", getClass().getSimpleName(), storeName);
+        System.out.println("wheeh..");
+    }
 }
