691edaeb2e1c945666151b48f5b8a2ad96a13d63
ivan <i.telyuk@lerna.tech>
2025-01-22 09:24:50 +0300

    add homework

---
+++ src/test/java/classwork/day21/DBProperties.java
@@ -0,0 +1,88 @@
+package classwork.day21;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class DBProperties {
+    private static final Properties PROP = getProperties();
+
+    public static void main(String[] args) {
+        //PROP.forEach((k, v) -> System.out.println(v.toString()));
+        //tring query = "SELECT * FROM Categories WHERE CategoryID = ?";
+        //execStatement(query);
+        //execParametrizedStatement(query,2);
+        loadCategories();
+    }
+
+    private static Properties getProperties() {
+        Properties prop = new Properties();
+        try (InputStream input = new FileInputStream("src/test/resources/db.properties")) {
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    private static MysqlDataSource getDataSource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setServerName(PROP.getProperty("HOST"));
+        dataSource.setPort(Integer.parseInt(PROP.getProperty("PORT")));
+        dataSource.setUser(PROP.getProperty("USER"));
+        dataSource.setPassword(PROP.getProperty("PWD"));
+        dataSource.setDatabaseName(PROP.getProperty("DBNAME"));
+        return dataSource;
+    }
+
+    private static void execStatement(String query) {
+        try (Connection connection = getDataSource().getConnection(); Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void execParametrizedStatement(String query, int id) {
+
+        try (Connection connection = getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+        ) {
+            statement.setInt(1, id);
+
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    System.out.println(rs.getString(2));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void loadCategories() {
+        String query = "SELECT * FROM Categories";
+        List<Category> categories = new ArrayList<>();
+
+        try (Connection connection = getDataSource().getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(query)) {
+
+            while (rs.next()) {
+                categories.add(new Category(rs.getInt(1), rs.getString(2), rs.getString(3)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        categories.forEach(category -> System.out.println(category.toString()));
+    }
+}
